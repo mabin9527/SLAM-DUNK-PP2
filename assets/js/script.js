d93223969b2a30620821877eba272ab882edb17e
mabin9527 <ma.bin9527000@gmail.com>
2022-12-11 12:43:31 +0000

    Use for loop to let every list to change its own style by adding extra className when mouse is over and leaving the icon

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//set up function to change style of nav-bar when click
+function changeCurrent() {
+    let navBar = document.querySelector('.nav-bar');
+
+    for (let i = 0; i < navBar.children.length; i++) {
+        navBar.children[i].addEventListener('mouseover', function() {
+            this.className = 'current';
+        })
+        navBar.children[i].addEventListener('mouseout', function() {
+            this.className = '';
+        })
+    }
+    
+}
+changeCurrent();
