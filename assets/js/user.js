f8950b3e9e8cc9dfba62fe140761716aa8b75adb
mabin9527 <ma.bin9527000@gmail.com>
2022-12-20 09:49:13 +0000

    Add else statement to create list to contain the comments

--- assets/js/user.js
+++ assets/js/user.js
@@ -14,17 +14,27 @@ changeCurrent();
 
 /**
  * Let the comments displayed on the comments-board. Use if else to judge whether clients leave 
- * the comments. If not, return an alert to remind user of leaving their thinking. 
+ * the comments. If not, return an alert to remind user of leaving their thinking. If user has 
+ * left a comment, create a list to fill in the content
  */
 
 let messege = document.querySelector('#messege');
 let btn = document.querySelector('#submit-btn');
 let commentBoard = document.querySelector('.comment-board');
 
-btn.addEventListener('click', function(){
-    if(messege == '') {
-        alert('Please type your comment');
+btn.onclick = function() {
+    if (messege.value === '') {
+        alert('Please type your feedback');
         return false;
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = messege.value + '<a href="javascript:;"><i class="fa-solid fa-trash"></i></a>';
+        commentBoard.insertBefore(li, commentBoard.children[0]);
+        let aLink = document.querySelectorAll('a');
+        for (let i in aLink) {
+            aLink[i].onclick = function() {
+                commentBoard.removeChild(this.parentNode);
+            }
+        }
     }
-    
-})
+}
